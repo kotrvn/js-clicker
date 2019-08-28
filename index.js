1212f71c64807daab3cc9bf417ea500aa7f65c78
unknown <kotrvn>
2019-08-28 16:19:28 +0300

    first commit

--- index.js
+++ index.js
@@ -1,120 +1,53 @@
-var $start = document.querySelector('#start');
-var $game = document.querySelector('#game');
-var $time = document.querySelector('#time');
-var $result = document.querySelector('#result');
-var $timeHeader = document.querySelector('#time-header');
-var $resulteHeader = document.querySelector('#result-header');
-var $gameTime = document.querySelector('#game-time');
+class Dropdown {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector);
+        this.items = options.items;
+        this.$el.querySelector('.dropdown__label').textContent = this.items[0].label;
 
+        this.$el.addEventListener('click', event => {
+            if (event.target.classList.contains('dropdown__label')) {
+                if (this.$el.classList.contains('open')) {
+                    this.close();
+                } else {
+                    this.open();
+                }
+                
+            } else if (event.target.tagName.toLowerCase() === 'li') {
+                this.select(event.target.dataset.id);
+            }
+        })
 
+        const itemsHTML = this.items.map( i => {
+            return `<li data-id='${i.id}'>${i.label}</li>`
+        }).join(' ');
 
-
-$start.addEventListener('click', startGame);
-$game.addEventListener('click', handleBoxClick);
-$gameTime.addEventListener('input', setGameTime);
-
-function show($el) {
-    $el.classList.remove('hide');
-
-}
-
-function hide($el) {
-    $el.classList.add('hide');
-}
-
-
-var score = 0;
-var isGameStarted = false;
-var colors = ['#009245 ', '#662D8C', '#00A8C5', '#D74177', '#ED1E79', '#FB872B', '#ED1C24', '#2E3192', '#8E78FF', '#00537E', '#4F00BC'];
-
-function startGame() {
-    score = 0;
-    
-    setGameTime();
-    $gameTime.setAttribute('disabled', true);
-    
-    isGameStarted = true;
-    $game.style.backgroundColor = '#fff';
-    hide($start);
-    
-    var interval = setInterval( function() {
-        var time = parseFloat($time.textContent)
-
-        if (time <= 0) {
-            clearInterval(interval);
-            endGame();
-        } else {
-            $time.textContent = (time - 0.1).toFixed(1);
-        }
-    }, 100);
-
-    renderBox();   
-}
-
-function setGameScore() {
-    $result.textContent = score.toString();
-}
-
-function setGameTime() {
-    var time = +$gameTime.value;
-    $time.textContent = time.toFixed(1);
-    show($timeHeader);
-    hide($resulteHeader);
-}
-
-function endGame() {
-    isGameStarted = false;
-    setGameScore();
-    $gameTime.removeAttribute('disabled');
-    show($start);
-    $game.style.backgroundColor = '#ccc';
-    $game.innerHTML = '';
-    hide($timeHeader);
-    show($resulteHeader);
-}
-
-function handleBoxClick(event) {
-    if (!isGameStarted) {
-        return;
+        this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin', itemsHTML)
     }
 
-    if(event.target.dataset.box) {
-        score++; 
+    open() {
+        this.$el.classList.add('open');
+    }
 
-        renderBox();
+    close() {
+        this.$el.classList.remove('open');
+    }
 
+    select(id) {
+        const item = this.items.find(i => i.id === id) 
+            this.$el.querySelector('.dropdown__label').textContent = item.label
+            this.close();
+        
     }
 }
 
 
-function renderBox() {
-   
-    $game.innerHTML = '';
-    var box = document.createElement('div');
-    var boxSize = getRandom(30, 100);
-    var gameSize = $game.getBoundingClientRect();
-    var maxTop = gameSize.height - boxSize;
-    var maxLeft = gameSize.width - boxSize;
-    var randomColorIndex = getRandom( 0, colors.length);
-
-    box.style.height = box.style.width = boxSize + 'px';
-    box.style.position = 'absolute';
-    box.style.backgroundColor = colors[randomColorIndex];
-    box.style.top = getRandom(0, maxTop) + 'px';
-    box.style.left = getRandom(0, maxLeft) + 'px';
-
-    box.style.cursor = 'pointer';
-    box.setAttribute('data-box', 'true');
-    $game.insertAdjacentElement("afterbegin", box);
 
 
-}
-
-
-
-
-
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
+const dropdown = new Dropdown('#dropdown', {
+    items: [
+        {label: 'Москва', id: 'msk'},
+        {label: 'Санкт-Петербург', id: 'spb'},
+        {label: 'Новосибирск', id: 'nsk'},
+        {label: 'Краснодар', id: 'krdr'}
+    ]
+});
